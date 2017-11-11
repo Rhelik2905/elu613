633c2e28c5a0f4e6c6d5e6c5364cad2f421a9d99
Hugo Le Blevec <hugo.leblevec@imt-atlantique.net>
2017-11-11 17:36:58 +0100

    we can now use a launch file

--- tp2_ros_package/scripts/keyboard_node.py
+++ tp2_ros_package/scripts/keyboard_node.py
@@ -6,32 +6,37 @@ from getch import *
 def talker():
     pub = rospy.Publisher('/cmd_vel_mux/input/navi', Twist, queue_size=1)
     rospy.init_node('keyboard_inputs_node', anonymous=True)
-    linspeed=0.1
+    linspeed=rospy.get_param("linspeed",default=0.1)
     msg=Twist()
     while not rospy.is_shutdown():
-    	char = getch()
-    	if char in [str(x) for x in range(1,10)]:
-    		linspeed = float(char)/10
-    	elif char == 't' or char == 'T':
-    		msg.linear.x=linspeed
-    		msg.angular.z=0.0
+    	char = getch() #retrieves the value of the pressed key.
+        linspeed=rospy.get_param("linspeed",default=0.1)
+        # We ask the robot to do different movements accordingly to the pressed key.
+    	if char in [str(x) for x in range(1,10)]: # If the key is 1-9 we set up the linear speed accordingly.
+            new_linspeed=float(char)/10
+            rospy.set_param("linspeed",new_linspeed)
+            rospy.set_param("fwd_linear_speed_x", new_linspeed) 
+            rospy.set_param("bckwd_linear_speed_x", -new_linspeed)
+    	elif char == 't' or char == 'T': # If the key is 'T' (or 't') we ask the robot to go straight forward at the set up linear speed.
+    		msg.linear.x=rospy.get_param("fwd_linear_speed_x",default=0.0)
+    		msg.angular.z=rospy.get_param("fwd_angular_speed_z",default=0.0)
     		rospy.loginfo(msg)
-    		pub.publish(msg)
-    	elif char == 'g' or char == 'G' :
-    		msg.linear.x=-linspeed
-    		msg.angular.z=0.0
-    		rospy.loginfo(msg)
-    		pub.publish(msg)
-    	elif char == 'h' or char == 'H':
-    		msg.linear.x=0.0
-    		msg.angular.z=-1.0
-    		rospy.loginfo(msg)
-    		pub.publish(msg)
-    	elif char == 'f' or char == 'F':
-    		msg.linear.x=0.0
-    		msg.angular.z=1.0
-    		rospy.loginfo(msg)
-    		pub.publish(msg)
+    		pub.publish(msg) 
+    	elif char == 'g' or char == 'G' : # If the key is 'G' (or 't') we ask the robot to go backward at the set up linear speed.
+            msg.linear.x=rospy.get_param("bckwd_linear_speed_x",default=0.0)
+            msg.angular.z=rospy.get_param("bckwd_angular_speed_z",default=0.0)
+            rospy.loginfo(msg)
+            pub.publish(msg)
+    	elif char == 'h' or char == 'H': # If the key is 'H' (or 'h') we ask the robot to do a clockwise rotation at 0.5 m/s of angular speed.
+            msg.linear.x=rospy.get_param("clw_linear_speed_x",default=0.0)
+            msg.angular.z=rospy.get_param("clw_angular_speed_z",default=0.0)
+            rospy.loginfo(msg)
+            pub.publish(msg)
+    	elif char == 'f' or char == 'F': # If the key is 'H' (or 'h') we ask the robot to do a counter-clockwise rotation at 0.5 m/s of angular speed.
+            msg.linear.x=rospy.get_param("counclw_linear_speed_x",default=0.0)
+            msg.angular.z=rospy.get_param("counclw_angular_speed_z",default=0.0)
+            rospy.loginfo(msg)
+            pub.publish(msg)
 
 
 
