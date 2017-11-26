95b4b341320fd4ab4bfa1c356011fd8c08616973
Seghaier Oussama <oussama.seghaier@gmail.com>
2017-11-26 19:16:25 +0100

    Update reactive_process.py

--- tp4_ros_package/scripts/reactive_process.py
+++ tp4_ros_package/scripts/reactive_process.py
@@ -6,8 +6,8 @@ from kobuki_msgs.msg import WheelDropEvent
 from kobuki_msgs.msg import MotorPower
 
 
-def BumperCallback(data):
-            
+def BumperCallback(data):  
+	# the global variable bump indicate if a bumper is pressed or not
 	global bump
         if (data.state == BumperEvent.PRESSED):
 	        bump = True
@@ -15,8 +15,8 @@ def BumperCallback(data):
                 bump = False
 
 def WheelDropCallback(data):
+	#the global variable wheel will indicate if a wheel is dropped or not
 	global wheel
-
 	if (data.state == WheelDropEvent.DROPPED):
 		wheel = True
 	else:
@@ -25,22 +25,33 @@ def WheelDropCallback(data):
 	
 
 def BumperListener():
+	
 	rospy.Subscriber("mobile_base/events/bumper",BumperEvent,BumperCallback)
+	# spin() simply keeps python from exiting until this node is stopped
 	rospy.spin()
 
 def WheelDropListener():
+	
 	rospy.Subscriber("mobile_base/events/wheel_drop",WheelDropEvent,WheelDropCallback)
+	# spin() simply keeps python from exiting until this node is stopped
 	rospy.spin()
 
 def MotorTalker(): 
 	
+	MotorON = MotorPower()
+	MotorON.state = Motor.ON
+	MotorOFF = MotorPower()
+	Motor.state = Motor.OFF
+	
 	pub = rospy.Publisher('mobile_base/commands/motor_power',MotorPower,queue_size=10)
 	rospy.init_node('MotorTalker', anonymous=True)
+	
 	BumperListener()
 	WheelDropListener()
+	
 	while not rospy.is_shutdown():
 		MotorON = MotorPower()
-		MOtorON.state = Motor.ON
+		MotorON.state = Motor.ON
 		MotorOFF = MotorPower()
 		Motor.state = Motor.OFF
 		if bump or wheel:
